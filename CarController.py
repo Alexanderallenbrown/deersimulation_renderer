2625c43fa7a8af0212c2c4e19fd829feff2ea21b
Alexander Brown <brownaa@lafayette.edu>
2019-03-28 16:17:31 -0400

    works

--- CarController.py
+++ CarController.py
@@ -5,7 +5,7 @@ import time
 from math import *
 #from numpy import *
 from bge import logic
-global  controller, car, cube, scene, f,k,initialP,flen,fname,carstate
+global  suspension,controller, car, cube, scene, f,k,initialP,flen,fname,carstate
 
 import os
 
@@ -13,18 +13,42 @@ import os
 k=0
 #set the name of the file that contains car positions
 fname = 'simdata.txt'
+carstate = [0,0,0,0,0,0]
+caray = 0
+carax = 0
 
+
+
+class Suspension:
+    def __init__(self,tauroll,taupitch):
+        self.tauroll = tauroll
+        self.taupitch = taupitch
+        self.roll = 0
+        self.pitch = 0
+
+    def update(self,dt,ax,ay):
+        self.roll+=dt/self.tauroll*((5.0*3.14/180)*ay/9.81-self.roll)
+        self.pitch+=dt/self.taupitch*((2.0*3.14/180)*-ax/9.81-self.pitch)
+        return self.roll,self.pitch
+
+suspension = Suspension(.2,.2)
 
 def procline(f):
-    global carstate
+    global carstate,caray,carax,suspension
     #the file for the car should have the following structure:
     # 0         1                   2             3  4  5  6  7     8        9       10     11    12     13        14            15             16        17     18
-    # t, steercommand,steerangle, X,U,Y,V,Psi,Psidot, Xdot,Udot,Ydot,Vdot,Psidot,Psiddot, deerspeed,deerpsi,deerx,deery
+    # t, steercommand,steerangle, Y,V,X,U,Psi,Psidot, Ydot,Vdot,Xdot,Udot,Psidot,Psiddot, deerspeed,deerpsi,deerx,deery
     line = f.readline()
     linestrip = line.strip()
     linesplit = line.split('\t')
-    if(len(linesplit)==19):
+    if(len(linesplit)==20):
         t,carstate = float(linesplit[0]), [float(linesplit[3]),float(linesplit[4]),float(linesplit[5]),float(linesplit[6]),float(linesplit[7]),float(linesplit[8])]
+        carstateder = [float(linesplit[9]),float(linesplit[10]),float(linesplit[11]),float(linesplit[12]),float(linesplit[13]),float(linesplit[14])]
+        #ay = Vdot + U*psidot
+        caray = carstateder[1]+carstate[3]*carstate[5]
+        #ax = Udot - V*psidot
+        carax = carstateder[3]-carstate[1]*carstate[5]
+        roll,pitch = suspension.update(1.0/60,carax,caray)
     else:
         print('Uh Oh. The file was not right')
 
@@ -37,7 +61,7 @@ def file_len(fname):
     return i + 1
 
 def setup():
-    global  controller, car, cube, scene, f, initialP,k,flen,fname,carstate
+    global  carax,caray,controller, car, cube, scene, f, initialP,k,flen,fname,carstate
     carstate = [0,0,0,0,0,0]
     #get the number of lines in this file so we know
     flen = file_len(fname)
@@ -46,7 +70,7 @@ def setup():
     #the file for the car should have the following structure:
     # t, X,U,Y,V,Psi,Psidot
     #get our initial car position out of the file
-   t, carstate = procline(f)
+    t, carstate = procline(f)
     #increment our counter. We will use this to loop through the file again if needed.
     k+=1
 
@@ -57,23 +81,26 @@ def setup():
     print(initialP)
 
     cube.worldPosition = [carstate[2],carstate[0],initialP[2]]
-    cube.worldOrientation = [0,0,car.x[4]]
+    cube.worldOrientation = [0,0,carstate[4]]
     scene = bge.logic.getCurrentScene()
 
 def main():
-    global  controller, car, cube, scene, f, initialP,k,flen,fname,carstate
+    global  carax,caray,controller, car, cube, scene, f, initialP,k,flen,fname,carstate
+    global suspension
     if(k<flen-1):
         t,carstate = procline(f)
         k+=1
     else:
         k=0
         f.close()
-        f.open()
+        f=open(fname,'r')
         t,carstate = procline(f)
 
     cube = controller.owner
     initialP = cube.worldPosition
     cube.worldPosition = [carstate[2],carstate[0],initialP[2]]
-    cube.worldOrientation = [0,0,carstate[4]]
+    # pitch = -(2.0*3.14/180)*carax/9.81
+    # roll = (5.0*3.14/180)*caray/9.81
+    cube.worldOrientation = [suspension.roll,suspension.pitch,carstate[4]]
     ## #state order is y,v,x,u,psi,r
     
--- DeerController.py
+++ DeerController.py
@@ -7,16 +7,20 @@ from bge import logic
 global  controller, Deer, cube, scene, f,k,initialP,flen,fname,deerstate
 
 fname = 'simdata.txt'
+k=0
+t=0
+deerstate=[0,0,0,0]
 
 def procline(f):
-    global deerstate
+    global deerstate,t
     #the file for the car should have the following structure:
     # 0         1                   2             3  4  5  6  7     8        9       10     11    12     13        14            15             16        17     18
-    # t, steercommand,steerangle, X,U,Y,V,Psi,Psidot, Xdot,Udot,Ydot,Vdot,Psidot,Psiddot, deerspeed,deerpsi,deerx,deery
+    # t, steercommand,steerangle, Y,V,X,U,Psi,Psidot, Ydot,Vdot,Xdot,Udot,Psidot,Psiddot, deerspeed,deerpsi,deerx,deery
     line = f.readline()
     linestrip = line.strip()
     linesplit = line.split('\t')
-    if(len(linesplit)==19):
+    # print(len(linesplit))
+    if(len(linesplit)==20):
         t,deerstate = float(linesplit[0]), [float(linesplit[15]),float(linesplit[16]),float(linesplit[17]),float(linesplit[18])]
     else:
         print('Uh Oh. The file was not right')
@@ -40,7 +44,7 @@ def setup():
     #the file for the car should have the following structure:
     # t, X,U,Y,V,Psi,Psidot
     #get our initial car position out of the file
-    t, carstate = procline(f)
+    t, deertstate = procline(f)
     #increment our counter. We will use this to loop through the file again if needed.
     k+=1
     controller = bge.logic.getCurrentController()
@@ -51,32 +55,32 @@ def setup():
 
 
 def main():
-    global  controller, Deer, deerstate, scene, f,k,initialP,flen,fname
-
+    global  t,controller, Deer, deerstate, scene, f,k,initialP,flen,fname
+    # print(t)
     if(k<flen-1):
         t,deerstate = procline(f)
         k+=1
     else:
         k=0
         f.close()
-        f.open()
+        f=open(fname,'r')
         t,deerstate = procline(f)
 
     Deer = controller.owner
     initialP = Deer.worldPosition
 
     Deer.worldPosition = [deerstate[2],deerstate[3],.7]
-    Deer.worldOrientation = [3.14/2,0,deerstate[1]]
+    Deer.worldOrientation = [0,0,3.14-deerstate[1]]
 
 def Running():
     global  controller, Deer, deerstate, scene, f,k,initialP,flen,fname
     controller = bge.logic.getCurrentController()
     scene = bge.logic.getCurrentScene()
     if deerstate[0] > 0:
-        print('RUNNING ANIMATION')
+        # print('RUNNING ANIMATION')
         controller.activate('Running')
         controller.deactivate('Standing')
     else:
-        print('NO RUNNING ANIMATION')
+        # print('NO RUNNING ANIMATION')
         controller.activate('Standing')
         controller.deactivate('Running')
